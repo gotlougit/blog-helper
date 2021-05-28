93cbdcf4dbedf93182e6125c0f00b49cab06474a
Saksham Mittal <gotlougit@users.noreply.github.com>
2021-05-28 17:37:05 +0530

    Added setup.py file for easy installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='blog-helper',
+      version='1.0',
+      description='Small blog maintenance helper',
+      long_description='Small program to add templates and add blogpost entry to main webpage with easy customization settings.',
+      author='Saksham Mittal',
+      author_email='gotlouemail@tutanota.com',
+      url='https://github.com/gotlougit/blog-helper',
+      packages=['blog-helper'],
+     )
