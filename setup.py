6eca8227874d69a4039e0724117bb7de575db78f
Saksham Mittal <gotlouemail@gmail.com>
2021-11-30 17:21:18 +0530

    Move some stuff around and improve packaging
    The setup.py file now utilizes entry points so there's no need to invoke
    python -m blog-helper, now we can just call blog-helper directly

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 
 setup(name='blog-helper',
-      version='1.0',
+      version='1.1',
       description='Small blog maintenance helper',
       long_description='Small program to add templates and add blogpost entry to main webpage with easy customization settings.',
       author='Saksham Mittal',
-      author_email='gotlouemail@tutanota.com',
+      author_email='gotlouemail@gmail.com',
       url='https://github.com/gotlougit/blog-helper',
-      packages=['blog-helper'],
+      scripts=['src/main.py', 'src/config.py', 'src/polish.py'],
+      entry_points = {
+        'console_scripts': [
+            'blog-helper = src.main:start'
+            ]
+          }
      )
